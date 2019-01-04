56ecd409e606e47fcfe1e802dabaf67718bfd721
petko-dimitrov <petko_dimitrov@abv.bg>
2019-01-04 17:00:17 +0200

    Added delete confirmation modal form for customers.

--- web/javascript/handleModal.js
+++ web/javascript/handleModal.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Delete visit
     $("#myModal").on('show.bs.modal', function(event){
         let button = $(event.relatedTarget);  // Button that triggered the modal
         let visitId = button.data('visit-id'); // Extract value from data-* attributes
@@ -8,12 +9,23 @@ $(document).ready(function(){
         $('#confirm-delete').attr('href', path);
     });
 
+    //Delete visit by customer
     $("#byCustomerModal").on('show.bs.modal', function(event){
         let button = $(event.relatedTarget);
         let visitId = button.data('visit-id');
         let customerName = button.data('customer-name');
         $('.modal-body').html(`<p>Do you really want to delete this visit to ${customerName}?</p>`);
         let path = `/visit/delete/${visitId}`;
+        $('#confirm-delete').attr('href', path);
+    });
+
+    //Delete customer
+    $("#customerModal").on('show.bs.modal', function(event){
+        let button = $(event.relatedTarget);
+        let customerId = button.data('customer-id');
+        let customerName = button.data('customer-name');
+        $('.modal-body').html(`<p>Do you really want to delete ${customerName}?</p>`);
+        let path = `delete/${customerId}`;
         $('#confirm-delete').attr('href', path);
     });
 });
